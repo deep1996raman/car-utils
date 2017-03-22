90a7a2fd4438130a0d0d2325ece47eeb9e827b8e
deep1996raman <ramaandeep003@gmail.com>
2017-03-22 15:48:20 +0530

    Add files via upload

---
+++ UserChoice.java
@@ -0,0 +1,11 @@
+
+public interface UserChoice {
+
+		int ADD_CUSTOMER=1;
+		int ADD_CAR_TO_CUSTOMER=2;
+		int LIST_CUSTOMER=3;
+		int LIST_INDIVIDUAL=4;
+		int GENERATE_PRIZES=5;
+		int EXIT=0;
+		
+}
